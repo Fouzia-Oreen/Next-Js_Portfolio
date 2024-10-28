b3aded4db1da92da10a34163021bb4b9104a5c3f
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-28 23:37:34 +0600

    Featured project added

--- nextjs-portfolio/src/app/page.tsx
+++ nextjs-portfolio/src/app/page.tsx
@@ -1,11 +1,17 @@
+import FeaturedProjectSection from "@/sections/FeaturedProject";
 import Header from "@/sections/Header";
-import HeroSection from "@/sections/HeroSection";
+import HeroSection from "@/sections/Hero";
+import TapeSection from "@/sections/Tape";
+import TestimonialSection from "@/sections/Testimonial";
 
 export default function Home() {
   return (
     <div >
      <Header />
      <HeroSection />
+     <FeaturedProjectSection />
+     <TapeSection />
+     <TestimonialSection />
     </div>
   );
 }
