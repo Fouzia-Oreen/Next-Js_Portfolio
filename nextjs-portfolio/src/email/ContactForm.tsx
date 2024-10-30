2d2085746ec194aa5325426f6e4f8d879b8e4ad2
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-30 22:27:53 +0600

    contact-updated

--- nextjs-portfolio/src/email/ContactForm.tsx
+++ nextjs-portfolio/src/email/ContactForm.tsx
@@ -27,13 +27,32 @@ import {
         <Tailwind>
           <Body className="bg-gray-100 text-black">
             <Container>
-              <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
-                <Heading className="leading-tight">
-                  You received the following message from the contact form
-                </Heading>
-                <Text>{message}</Text>
-                <Hr />
-                <Text>The sender&apos;s email is: {senderEmail}</Text>
+              <Section className="bg-sky-100  my-10 px-6 py-4 rounded-md">
+              <Heading className="leading-tight">
+              Subject: This is a demo email from {senderEmail}
+              </Heading>
+              <Text className="text-gray-950">
+              Dear John Doe,
+              <br/>
+              Thank you for expressing interest in [Your Software] during our recent conversation. I’m excited to share how our software can help you achieve [Specific Benefit] and streamline your operations.
+              <br/>
+              To help you better understand our solution, I’d like to invite you to an exclusive live demo of [Your Software]. During this session, we will walk you through the key features and answer any questions you may have.
+              <br/>
+              {message}
+              <br/>
+              <br/>
+              Looking forward to showcasing the value of [Your Software] to your business!
+              <br/>
+              <br/>
+              Best regards,
+              <br/>
+              John Doe
+              <br/>
+              {senderEmail}
+              <br/>
+              +000 1223 3345 00
+              </Text>
+              <Hr />
               </Section>
             </Container>
           </Body>
